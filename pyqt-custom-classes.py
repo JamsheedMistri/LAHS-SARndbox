fd47469be11dcf202c4f6b6c99ef467d6604985b
Adam Weingram <adamweingram@gmail.com>
2018-05-29 13:46:28 -0700

    ACTUALLY add a file for custom QT classes (screwed up the last commit)

---
+++ pyqt-custom-classes.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import *
+
+# Example function
+def foo():
+    print("foo!")
+
+
+# Class for simple prompting for options
+class Choice(QWidget):
+    def __init__(self, question, window_title, options):
+        super().__init__()
+        buttons = []
+        self.initUI(question, window_title, options, buttons)
+        print(buttons)
+
+    def initUI(self, question, window_title, options, buttons):
+        grid = QGridLayout()
+        grid.setSpacing(10)
+        grid.addWidget(QLabel(question), 1, 0)
+
+        for index, option in enumerate(options):
+            new_button = QPushButton(option[0])
+            new_button.clicked.connect(option[1])
+            buttons.append(new_button)
+            grid.addWidget(new_button, index + 2, 0)
+
+        self.setLayout(grid) 
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle(window_title)
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Choice("Would you like to run the sandbox?", "Testing", [["Ok", foo], ["Cancel", foo]])
+    sys.exit(app.exec_())
